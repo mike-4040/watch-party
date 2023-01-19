e92779a13adb9170b9de4f963f3e83b74985d892
Mike Kravtsov <mike4040@me.com>
2023-01-19 10:53:48 -0500

    api prewire

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 import { DEFAULT_PORT } from './constants.js';
+import { health } from './middleware/health.js';
 
 const port = process.env.PORT || DEFAULT_PORT;
 
 express()
-  .get('/', (req, res) => {
-    res.send('Hello World!');
-  })
+  .use(cors())
+  .get('/health', health)
   .listen(port, () => console.log(`Listening on http://localhost:${port}`));
---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+import type { ThemeOptions } from '@mui/material';
+
+export const BASE_URL = 'http://localhost:3001';
+
+export const THEME: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+  },
+} as const;
